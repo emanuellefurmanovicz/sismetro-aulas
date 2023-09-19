9aed2f28f9af7500fddc676de9a2be2dd0d9446c
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2023-09-19 16:00:36 -0300

    aula13

---
+++ aula13/aula13.js
@@ -0,0 +1,17 @@
+function soma(n1,n2){
+    return n1 + n2;
+}
+function subtraçao(n1,n2){
+    return n1 - n2
+}
+function divisao(n1,n2){
+    return n1 / n2
+}
+function multiplicaçao(n1,n2){
+    return n1 * n2
+}
+
+function somar(){
+   document.getElementById"resultado").value = soma(Number(document.getElementById("n1").value),)
+    Number(document.getElementById("n2").value));
+}
