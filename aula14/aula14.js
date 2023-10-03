93d5c5b00b4bb7887ab2e5e1c49e20da2729ff66
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2023-10-03 16:11:06 -0300

    aula 14 hhhhhh

--- aula14/aula14.js
+++ aula14/aula14.js
@@ -83,3 +83,56 @@ function dividir() {
     document.getElementById("resultado").value = divisao(Number(document.getElementById("n1").value),
        Number (document.getElementById("n2").value));
 }
+function apagar(){
+    document.getElementById("valor").value=document.getElementById("valor").value.substring(0,document.getElementById("valor").value.length-1)
+}
+function deletar() { 
+    document.getElementById("valor").value=document.getElementById("valor").value.substring(0,document.getElementById("valor").value.length-document.getElementById("valor").value.length)
+}
+function adicionar_ponto(){ 
+    if(document.getElementById("valor").value.includes("."))
+    document.getElementById("valor").value =
+    document.getElementById("valor").value.concat(".")
+}
+function porcentagem(){
+    if(typeof valor == typeof undefined) {
+        document.getElementById("valor").value =
+        Number(document.getElementById("valor").value)/100;
+    }else{
+    document.getElementById("valor").value =
+    (valor/100) *
+    Number(document.getElementById("valor").value);
+}
+}
+
+function resultado() {
+    document.getElementById("valor").value
+} 
+
+function addNumero(){
+    valor = addnum(Number(document.getElementById('valor').value))
+}
+function menosNumero(){
+    valor = menosnum(Number(document.getElementById('valor').value))
+}
+function apagarMemoria(){
+    memoria = 0
+    return console.log(memoria)
+}
+function mostrarMemoria(){
+    document.getElementById('valor').value = memoria
+}
+function resultado(){
+    document.getElementById('valor').value = operacao(valor, Number(document.getElementById('valor').value))
+
+    valor = undefined
+    operacao = undefined
+}
+function addnum(n1){
+    memoria += n1
+    return console.log(memoria)
+}
+function menosnum(n1){
+    memoria -= n1
+    return console.log(memoria)
+}
